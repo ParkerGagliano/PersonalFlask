6cf536c5ecbf0b61955926729af2b8d5615967c7
Parker <pjg4086@uncw.edu>
2022-09-14 18:46:35 -0400

    fist commit

---
+++ README.md
@@ -0,0 +1,6 @@
+in order to run:
+export FLASK_APP = 'personalflask.py'
+export DEBUG_MODE = 1
+flask run -h localhost -p 3000
+
+running on 3000 becase mac is weird
---
+++ personalflask.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Parker Gagliano',
+        'title': 'Bottlecap for sale',
+        'content': 'this is a rare collectable bottlecap',
+        'date_posted': 'April 20, 2022'
+    },
+    {
+        'author': 'Parker Gagliano',
+        'title': 'stamp for sale(rare, real)',
+        'content': 'this is a rare collectable stamp',
+        'date_posted': 'April 1, 2022'
+    }
+]
+
+
+@app.route("/home")
+def hello():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=1)
