32385756622e48400d19df7564c6167f90ba36b9
Parker <pjg4086@uncw.edu>
2022-09-16 17:22:47 -0400

    registration form

--- personalflask.py
+++ personalflask.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'b0840af5a08f99cbd53c13fe64773dfd'
 
 
 posts = [
@@ -19,13 +22,28 @@ posts = [
 
 
 @app.route("/home")
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
     return render_template('about.html', title="Project")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
