6c88f686111ccf3243e12c34da96750a16912ef5
Parker <pjg4086@uncw.edu>
2022-09-23 17:18:10 -0400

    register and login

--- personalflask.py
+++ personalflask.py
@@ -40,9 +40,15 @@ def register():
     return render_template('register.html', title='register', form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'joesmith@gmail.com' and form.password.data == 'password':
+            flash('you have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('login unsuccessful. please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
